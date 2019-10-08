3b63390485475be2058fae7970801dd8d99baed3
Damian Arya <dahakcyrus@yahoo.com>
2019-10-08 13:00:44 -0600

    added info and started info flow

--- epic/index.php
+++ epic/index.php
@@ -5,13 +5,27 @@
 	<title>data-design</title>
 </head>
 <body>
+<h1> as a Grubhub user I want to order food to my party at any cost</h1>
+
 <div>
-	Jesse pinkman
+	<Jesse pinkman
 </div>
+<br>
+<br>
+
 <img src="download.jpg">
 <ul>
 	<li>lives in albuquerque</li>
-	<li>I like to through large partys </li>
+	<li>I like to throw large partys </li>
 	<li>I have more money then I know what to do with </li>
 	<li>I do not drive or own a vehical</li>
+	<li> I am very lazy, if there is an easyer way, I'll do that.</li>
+	<li> As a lazy human I want to order food with ease and price does not matter to me.</li>
+	<li>I keep odd work hours and sleeping schedual</li>
+	<li>great at cooking but not great with food</li>
 </ul>
+<br>
+<br>
+<h2> use case and interaction flow</h2>
+
+</body>
