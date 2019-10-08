a0b27cfd6f8e300cecd347c6636705ba92a1108e
Damian Arya <dahakcyrus@yahoo.com>
2019-10-08 10:17:58 -0600

    creadted document

---
+++ epic/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>data-design</title>
+</head>
+<body>
+<div>
+	Jesse pinkman
+</div>
+<img src="download.jpg">
+<ul>
+	<li>lives in albuquerque</li>
+	<li>I like to through large partys </li>
+	<li>I have more money then I know what to do with </li>
+	<li>I do not drive or own a vehical</li>
+</ul>
