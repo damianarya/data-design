64287e2dbd3210837c16698a2bc6c2e1d9033afc
Damian Arya <dahakcyrus@yahoo.com>
2019-10-08 13:59:15 -0600

    added css

--- epic/index.php
+++ epic/index.php
@@ -3,29 +3,43 @@
 <head>
 	<meta charset="UTF-8">
 	<title>data-design</title>
+	<link rel="stylesheet" type="text/css" href="./data-design.css">
 </head>
 <body>
-<h1> as a Grubhub user I want to order food to my party at any cost</h1>
-
+<h1> persona:</h1>
+<br>
+<P> I'm Jesse Pinkman. I live in albuquerque. I'm average to below average tech and computer user.
+<br> I lke to throw large partys and have more money then I know what to do with.
+<br> I do not drive or have a car and I'm very lazy, if its easyer then thats the way for me.
+<br> I keep odd work and sleeping hours and I'm famous for my cooking skills but I dislike making my own food.</P>
 <div>
-	<Jesse pinkman
-</div>
-<br>
-<br>
 
 <img src="download.jpg">
+	<br>
+	<h2> User story:</h2>
+	<br>
+	<p>as a grubhub user I want food brought to me now, I dont care about expence</p>
+	<br>
+	<h3> Use case/ Interaction</h3>
+	<br>
+	<p> Jesse is having a huge party that has gone on for days and everything is fine but people are starting to pass out
+	<br> presumably from hunger. this needs to be fixed as soon as it can be.</p>
+	<br>
+	<p><i>precondition:</i> everyone is hungry and unhappy about it.</p>
+	<p><i>postcondition:</i> everyone is fed and back to partying harty.</p>
+	<br>
+
 <ul>
-	<li>lives in albuquerque</li>
-	<li>I like to throw large partys </li>
-	<li>I have more money then I know what to do with </li>
-	<li>I do not drive or own a vehical</li>
-	<li> I am very lazy, if there is an easyer way, I'll do that.</li>
-	<li> As a lazy human I want to order food with ease and price does not matter to me.</li>
-	<li>I keep odd work hours and sleeping schedual</li>
-	<li>great at cooking but not great with food</li>
+	<li>Jesse opens his grubhub app by tapping the eyecon on his mobile device</li>
+	<li>he is presented with a list of resturants that are currently open</li>
+	<li>he selects the resturaunt for him by clicking (or tapping) on it</li>
+	<li>he then selects the exact food options he wants by clicking on them </li>
+	<li>he is presented with a price break down, he agrees to it by tapping a "pay now button"</li>
+	<li>he enters his payment info via a prompt screen</li>
+	<li>finally he compleates the order by tapping a "compleat order" button</li>
+	<li>he is then presented with a page detailing how long it will take for his order to arrive</li>
+	<li>the order arrives and all is well</li>
 </ul>
-<br>
-<br>
-<h2> use case and interaction flow</h2>
 
 </body>
+</html>
